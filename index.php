a04ce682298396d574c419875951136c8cf34117
Tim Hosey <insertcointheater@gmail.com>
2024-09-21 00:43:12 -0700

    initial build

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
 <head>
+  <title>
+    Timcade!
+  </title>
+  <link rel="stylesheet" href="style.css" />
+  <script src="https://unpkg.com/@tryghost/content-api@1.11.21/umd/content-api.min.js"></script>
+  <script src="blog.js"></script>
 </head>
-<body class="center bg2 max">
+<body class="center bg1 max">
 <div id="main">
-<h1 class="tab blink">Welcome to my website!</h1>
+<h1 class="tab">Timcade</h1>
 <div id="home">
 <h4><a href="#home">Home</a> <span class="line">|</span>
 <a href="#about">About Me</a> <span class="line">|</span>
 <a href="#working">Projects I am Working On</a> <span class="line">|</span>
 <a href="#made">Stuff I Made</a></h4>
 </div>
-<img class="intro" id="about" src="https://i.imgur.com/VzhrVJt.gif">
+<div class="intro blink">LOGO HERE</div>
 <h2>This is the base file for the 90s themed website I want to make</h2>
-<h2 id="working">Projects I am working on</h2>
-<p></p>
-<h2 id="made">Stuff I made</h2>
+<h2 id="working">My Projects</h2>
+<p>Twitch Stream | YouTube Channel</p>
+<h2 id="made">Social Media</h2>
+<p><a href="https://meh.social/@timcade">Mastodon</a> | <a href="https://www.instagram.com/TheTimcade/">Instagram</a> | <a href="https://tiktok.com/@thetimcade">TikTok</a></p>
+<h2 id="blog">Latest Blog Posts</h2>
+<p id="blogPost"></p>
 </div>
 <footer id="footer">
-<img src="https://i.imgur.com/i6hvJjx.gif">
-<p>This is a link to my <a href="http://portfolio.coolandgood.org">portfolio</a></p>
-<span><img src="https://i.imgur.com/QkBkUkC.gif"></span>
+<img src="img/theme_div.gif">
+<!-- https://i.imgur.com/i6hvJjx.gif -->
+<p>Copyright &copy; 2024 Timcade</p>
+<span><img src="img/webring.gif"></span>
 </footer>
 </body>
