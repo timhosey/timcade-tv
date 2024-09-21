cfffbc43e7b6d8520336f7c0fdc3c9fe719d0ac8
Tim Hosey <insertcointheater@gmail.com>
2024-09-21 02:08:28 -0700

    added intiial about page. want to add my photo at some point

---
+++ about.php
@@ -0,0 +1,44 @@
+<?php require_once 'head.php'; ?>
+    <h2 id="working">About Tim</h2>
+    <p>
+        Hi, I'm Tim. I'm a nerd. Not just a nerd, but what some would describe, in the 1990's parlance, an übernerd.
+        If we're assigning labels, geek, Japan fan, and gamer would also apply, but labels are only one dimension of
+        a multifaceted person.
+    </p>
+
+    <p>
+        I've been a content creator since 2015, when I started a small Podcast called "Insert Coin Theater" with my
+        friend Jorge. From there, I branched into live streaming under the same name. Streaming on Twitch and making
+        Let's Play style videos on YouTube were my ultimate hobby, one that I pushed toward becoming a career. Once I
+        made my move to Microsoft's Mixer platform (Beam at the time I joined), things were really looking up and
+        accelerating. I began a new podcast called Mandatory Social Interaction Show (MSIS) with my content creating
+        artist friend <a href="https://abstractionmusic.com" target="_new">Ben Burnes.</a> I started a company called
+        RetroFizz with my content creating nerd friends, Sean and Tim T. Our primary goal was to create the first 24/7
+        livestream with curated, specially made content exclusive to the Mixer Platform. We facilitated show scheduling,
+        designed premiere dates and collaborated (and paid!) fellow creators to produce content for our channel. Things
+        only fell apart because Mixer announced its closure, which dried up our only target market at the time (we had
+        not yet had the chance to execute to Twitch). As such, we parted ways, a success in its own rite and a worthwhile
+        experiment.
+    </p>
+
+    <p>
+        Once Mixer dissolved, we found ourselves in the midst of a pandemic, and I found myself, in essence, starting fresh
+        with my content. I was burnt out, on top of it. Working a fulltime job, on top of fulltime content creation for
+        years on end, had taken its toll. I needed a break, and I needed something fresh. So, I rebranded as Timcade, moved
+        to the Pacific Northwest, and joined a local LPFM radio station. This lead in turn to my current incarnation of
+        content creation. I am seeking to find the intersection between all my interests: games, tech, and Japan. It's going
+        to be a journey to find the sweet spot, but I'm hopeful you'll join me in discovering just what the intersecting
+        point looks like.
+    </p>
+    <p>
+        I'm really grateful you've taken the time to read this far. If you like my writing style, consider looking at
+        <a href="https://simplytim.io">my blog</a>, where I write techy stuff and sometimes random pontificating about
+        geeky topics. You may also find it worthwhile to <a href="https://linkedin.com/in/timhosey">connect with me on
+        LinkedIn</a>. I'm no <a href="https://reddit.com/r/LinkedInLunatics" target="_new">lunatic</a>, but I do try to
+        share when I write something that might be helpful to others in my industry area (DevOps Consulting).
+    </p>
+    <p>
+        I also share stuff on my <a href="https://meh.social/@timcade" target="_new">Mastodon instance</a>
+        if you're a fellow Mastodon user! You can also sign up for my Mastodon isntance on that link if you'd like.
+    </p>
+<?php require_once 'foot.php'; ?>
