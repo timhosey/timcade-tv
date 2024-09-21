0f8041cf7d1280444109f53b3d55db19970a36f1
Tim Hosey <insertcointheater@gmail.com>
2024-09-21 02:08:50 -0700

    footer and header break out php files

---
+++ foot.php
@@ -0,0 +1,11 @@
+</div>
+  <footer id="footer">
+    <img src="img/theme_div.gif">
+    <!-- https://i.imgur.com/i6hvJjx.gif -->
+    <p>Content &copy; 2024 Timcade
+      <br />Design based on <a href="https://codepen.io/nikillpop/pen/VdJjJW">nikillpop's</a>
+    </p>
+    <span><img src="img/webring.gif"></span>
+  </footer>
+  <script src="blog.js"></script>
+</body>
---
+++ head.php
@@ -0,0 +1,22 @@
+<head>
+  <title>
+    Timcade!
+  </title>
+  <link rel="stylesheet" href="style.css" />
+  <script src="https://unpkg.com/@tryghost/content-api@1.11.21/umd/content-api.min.js"></script>
+</head>
+<body class="center bg1 max">
+  <div id="main">
+    <div class="home">
+      <h4>
+        <a href="index.php">Home</a> <span class="line">|</span>
+        <a href="about.php">About Me</a> <span class="line">|</span>
+        <a href="https://github.com/timhosey" target="_new">My GitHub</a>
+      </h4>
+    </div>
+    <h1 class="tab">You've Found...</h1>
+    <div class="intro blink">TIMCADE</div>
+    <p>(logo coming soon)</p>
+    <div>
+      <img src="img/theme_div.gif">
+    </div>
