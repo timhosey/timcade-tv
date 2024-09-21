37d96aadab218a2401154f242c2c4eb6a0e8e2b4
Tim Hosey <insertcointheater@gmail.com>
2024-09-21 02:31:38 -0700

    added webring link to foot

--- foot.php
+++ foot.php
@@ -5,7 +5,7 @@
     <p>Content &copy; 2024 Timcade
       <br />Design based on <a href="https://codepen.io/nikillpop/pen/VdJjJW">nikillpop's</a>
     </p>
-    <span><img src="img/webring.gif"></span>
+    <span><a href="webring.php"><img src="img/webring.gif"></a></span>
   </footer>
   <script src="blog.js"></script>
 </body>
