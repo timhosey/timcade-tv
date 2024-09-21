7d3d57ccef3f866728417f5d7f5b68d3bcfc3c14
Tim Hosey <insertcointheater@gmail.com>
2024-09-21 02:08:59 -0700

    add header and footer includes

--- index.php
+++ index.php
@@ -1,33 +1,8 @@
-<head>
-  <title>
-    Timcade!
-  </title>
-  <link rel="stylesheet" href="style.css" />
-  <script src="https://unpkg.com/@tryghost/content-api@1.11.21/umd/content-api.min.js"></script>
-  <script src="blog.js"></script>
-</head>
-<body class="center bg1 max">
-<div id="main">
-<h1 class="tab">Timcade</h1>
-<div id="home">
-<h4><a href="#home">Home</a> <span class="line">|</span>
-<a href="#about">About Me</a> <span class="line">|</span>
-<a href="#working">Projects I am Working On</a> <span class="line">|</span>
-<a href="#made">Stuff I Made</a></h4>
-</div>
-<div class="intro blink">LOGO HERE</div>
-<h2>This is the base file for the 90s themed website I want to make</h2>
-<h2 id="working">My Projects</h2>
-<p>Twitch Stream | YouTube Channel</p>
-<h2 id="made">Social Media</h2>
-<p><a href="https://meh.social/@timcade">Mastodon</a> | <a href="https://www.instagram.com/TheTimcade/">Instagram</a> | <a href="https://tiktok.com/@thetimcade">TikTok</a></p>
-<h2 id="blog">Latest Blog Posts</h2>
-<p id="blogPost"></p>
-</div>
-<footer id="footer">
-<img src="img/theme_div.gif">
-<!-- https://i.imgur.com/i6hvJjx.gif -->
-<p>Copyright &copy; 2024 Timcade</p>
-<span><img src="img/webring.gif"></span>
-</footer>
-</body>
+<?php require_once 'head.php'; ?>
+    <h2 id="working">Content & Community</h2>
+    <p><a href="https://twitch.tv/Timcade" target="_new">Twitch Stream</a> | <a href="https://youtube.com/Timcade" target="_new">YouTube Channel</a> | <a href="https://discorg.gg/thearcade" target="_new">Community Discord</a></p>
+    <h2 id="made">Social Media</h2>
+    <p><a href="https://meh.social/@timcade" target="_new">Mastodon</a> | <a href="https://www.instagram.com/TheTimcade/" target="_new">Instagram</a> | <a href="https://tiktok.com/@thetimcade" target="_new">TikTok</a></p>
+    <h2 id="blog">Latest Blog Posts</h2>
+    <p id="blogPost"></p>
+<?php require_once 'foot.php'; ?>
