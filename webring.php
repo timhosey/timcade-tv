8f583b96c287a563b7021a28c81c22a8bea7ebcb
Tim Hosey <insertcointheater@gmail.com>
2024-09-21 02:31:17 -0700

    add webring page

---
+++ webring.php
@@ -0,0 +1,10 @@
+<?php require_once 'head.php'; ?>
+    <h2 id="working">Timcade Webring</h2>
+    <p>
+      A webring is a group of sites that link to each other because they like what they make.
+    </p>
+
+    <p>
+      <a href="https://abstractionmusic.com" target="_new">Abstraction Music</a>
+    </p>
+<?php require_once 'foot.php'; ?>
