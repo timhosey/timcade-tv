54f123abd4c39cbea72081bf35ce812afd90a815
Tim Hosey <insertcointheater@gmail.com>
2024-09-24 18:02:15 -0700

    added emfed for mastodon post pull

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <?php require_once 'head.php'; ?>
+
     <h2 id="working">Content & Community</h2>
-    <p><a href="https://twitch.tv/Timcade" target="_new">Twitch Stream</a> | <a href="https://youtube.com/Timcade" target="_new">YouTube Channel</a> | <a href="https://discorg.gg/thearcade" target="_new">Community Discord</a></p>
-    <div><iframe class="latestVideoEmbed" vnum='0' width="600" height="340" frameborder="0" allow="fullscreen;"></iframe></div>
+    <p>
+        <a href="https://twitch.tv/Timcade"
+             target="_new">Twitch Stream</a> |
+        <a href="https://youtube.com/Timcade"
+             target="_new">YouTube Channel</a> |
+        <a href="https://discorg.gg/thearcade"
+             target="_new">Community Discord</a>
+    </p>
+    <div>
+        <iframe class="latestVideoEmbed"
+             vnum='0'
+             width="600"
+             height="340"
+             frameborder="0"
+             allow="fullscreen;">
+        </iframe>
+    </div>
+
     <h2 id="made">Social Media</h2>
-    <p><a href="https://meh.social/@timcade" target="_new">Mastodon</a> | <a href="https://www.instagram.com/TheTimcade/" target="_new">Instagram</a> | <a href="https://tiktok.com/@thetimcade" target="_new">TikTok</a></p>
+    <p>
+        <a href="https://meh.social/@timcade"
+             target="_new">Mastodon</a> |
+        <a href="https://www.instagram.com/TheTimcade/"
+             target="_new">Instagram</a> |
+        <a href="https://tiktok.com/@thetimcade"
+             target="_new">TikTok</a>
+    </p>
+    <p>
+        <a class="mastodon-feed"
+            href="https://meh.social/@Timcade"
+            data-toot-limit="1"
+            data-exclude-replies="true"
+            data-exclude-reblogs="true">
+            follow me into the Fediverse
+        </a>
+    </p>
+
     <h2 id="blog">Latest Blog Posts</h2>
     <p id="blogPost"></p>
 
     <script src="youtubevideo.js"></script>
+    <script type="module" src="https://esm.sh/emfed@1"></script>
 <?php require_once 'foot.php'; ?>
