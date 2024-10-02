01a20bcb6a4d95579c081baa925e4289c75f9ead
Tim Hosey <insertcointheater@gmail.com>
2024-10-01 17:43:57 -0700

    added hitcounter

--- foot.php
+++ foot.php
@@ -5,7 +5,7 @@
     <p>Content &copy; 2024 Timcade
       <br />Design based on <a href="https://codepen.io/nikillpop/pen/VdJjJW" alt="link to nikillpop's codepen">nikillpop's</a>
     </p>
-    <span><a href="webring.php"><img src="img/webring.gif" alt="webring image" /></a></span>
+    <span><a href="webring.php"><img src="img/webring.gif" alt="webring image" /></a> &middot; <script type="text/javascript" src="//counter.websiteout.com/js/24/4/0/1"></script></span>
   </footer>
   <script src="blog.js"></script>
 </body>
