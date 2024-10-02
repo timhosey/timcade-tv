6cf215648abb4a1130a46a68b891c79925d4c365
Tim Hosey <insertcointheater@gmail.com>
2024-10-01 18:32:47 -0700

    updated footer and stylized it to accomodate counter best

--- foot.php
+++ foot.php
@@ -5,7 +5,10 @@
     <p>Content &copy; 2024 Timcade
       <br />Design based on <a href="https://codepen.io/nikillpop/pen/VdJjJW" alt="link to nikillpop's codepen">nikillpop's</a>
     </p>
-    <span><a href="webring.php"><img src="img/webring.gif" alt="webring image" /></a> &middot; <script type="text/javascript" src="//counter.websiteout.com/js/24/4/0/1"></script></span>
+    <div class="info">
+      <a href="webring.php"><img src="img/webring.gif" alt="webring image" /></a>
+      <br /><script type="text/javascript" src="//counter.websiteout.com/js/24/4/0/1"></script>
+    </div>
   </footer>
   <script src="blog.js"></script>
 </body>
