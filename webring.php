6153cd9e10930b5a232f9420bbc9455e37c4aec8
Tim Hosey <insertcointheater@gmail.com>
2024-11-12 19:57:45 -0800

    added Nina's links

--- webring.php
+++ webring.php
@@ -5,7 +5,11 @@
     </p>
 
     <p>
-      <a href="https://abstractionmusic.com" target="_new" alt="Link to Abstraction Music dot com">Abstraction Music</a> - This is the legendary site of ben_burnes,
-      one of my favorite musicians, game devs, and friendos.
+      <a href="https://abstractionmusic.com" target="_new" alt="Link to Abstraction Music dot com">Abstraction Music</a>
+      <br />This is the legendary site of ben_burnes, one of my favorite musicians, game devs, and friendos.
+    </p>
+    <p>
+      <a href="https://misnina.com" target="_new" alt="Link to Nina's Art Page"><img src="img/misnina.jpg" /></a>
+      <br />This is the artist who made the Timcade emotes on Discord and Twitch. Commission her!
     </p>
 <?php require_once 'foot.php'; ?>
